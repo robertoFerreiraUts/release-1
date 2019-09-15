81b9d421e942f0381c49a540b3bf23c6c39cb524
jackson rajbhandari <jacksonrajbhandari14@gmail.com>
2019-09-15 19:09:22 +1000

    updated index and products

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+
+
+
+var indexRouter = require('./routes/index');
 
 const app = express();
 //DB config
@@ -17,17 +23,25 @@ const users = require('./routes/users');
 
 const delivery = require('./routes/delivery');
 
+
+
 // Passport Config
 require('./config/passport')(passport);
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
+
+
 // Connect to mongoose
-mongoose.connect(db.mongoURI, {
-  useMongoClient: true
-})
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopping', {useNewUrlParser: true});
+
+// puttt 
+
+  
+
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -38,6 +52,9 @@ app.set('view engine', 'handlebars');
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -68,12 +85,12 @@ app.use(function(req, res, next){
 });
 
 // Index Route
-app.get('/', (req, res) => {
-  const title = 'Welcome';
-  res.render('index', {
-    title: title
-  });
-});
+//app.get('/', (req, res) => {
+ // const title = 'Welcome';
+ //res.render('index', {
+ // title: title
+//  });
+//});
 
 // About Route
 app.get('/about', (req, res) => {
@@ -99,6 +116,8 @@ app.use('/users', users);
 
 app.use('/delivery', delivery);
 
+app.use('/', indexRouter);
+
 //Load 404 page (if page is not exist!)
 app.use((req ,res) => res.render('not_found'));
 
@@ -108,3 +127,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
+
+module.exports = app;
