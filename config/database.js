e95ac699926fdcfa981f91857eebb71ce4320b65
dos203 <dinoshanr@gmail.com>
2019-10-01 19:23:05 +1000

    Recover

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI:
-        'mongodb://users:Test123@ds157946.mlab.com:57946/heroku_tlms4x2k'}
-        // mongodb://newdown:newdown1234@ds263927.mlab.com:63927/heroku_fw9f58vn
+        ' mongodb://newdown:newdown1234@ds263927.mlab.com:63927/heroku_fw9f58vn'}
+        // mongodb://users:Test123@ds157946.mlab.com:57946/heroku_tlms4x2k
+       
        
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/LocalDev'}
