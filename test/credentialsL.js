86a7109ae38883761ae0f2bfbe7a51b8fa7cae05
dos203 <dinoshanr@gmail.com>
2019-10-12 19:32:29 +1100

    Auto Test

---
+++ test/credentialsL.js
@@ -0,0 +1,8 @@
+module.exports ={
+    firstname: "testfirstname",
+    lastname: "testlastname",
+    email: 'e2@mail.com',
+    email2: 'e2@mail.com',
+    password: 'asdasd',
+    password2: "asdasd"
+}
