82d7cfe9848e42c46f970e6f35bf01a22fa78cb6
brigzap <49735180+brigzap@users.noreply.github.com>
2019-10-16 09:40:22 +1100

    test

---
+++ seed/Recipes-seeder.js
@@ -0,0 +1,55 @@
+var Recipe = require('../models/Recipes');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/Recipes',{useNewUrlParser:true});
+
+var Recipes = 
+[
+
+    new Recipe({
+        recipe_ID: 1,
+        recipe_Name: 'Butter Chicken',
+        item: '600g Chicken, 1/2 cup Yoghurt, 410g Tomato puree, 1/2 cup Cream, 1 Onion, 2 cloves Garlic, 1/4 teaspoon chilli powder',
+        instructions: '1. Place yoghurt, garlic and chilli powder into a dish. 2. Add chicken, stir to coat and refridgerate for 2 hours',
+        image: 'image/recipe1.jpg'
+    }),
+
+    new Recipe({
+        recipe_ID: 2,
+        recipe_Name: 'Chocolate Cake',
+        item: '600g Chicken, 1/2 cup Yoghurt, 410g Tomato puree, 1/2 cup Cream, 1 Onion, 2 cloves Garlic, 1/4 teaspoon chilli powder',
+        instructions: '1. Place yoghurt, garlic and chilli powder into a dish. 2. Add chicken, stir to coat and refridgerate for 2 hours',
+        image: 'image/recipe2.jpg'
+    }),
+
+    new Recipe({
+        recipe_ID: 3,
+        recipe_Name: 'Spaghetti Bolognese',
+        item: '600g Chicken, 1/2 cup Yoghurt, 410g Tomato puree, 1/2 cup Cream, 1 Onion, 2 cloves Garlic, 1/4 teaspoon chilli powder',
+        instructions: '1. Place yoghurt, garlic and chilli powder into a dish. 2. Add chicken, stir to coat and refridgerate for 2 hours',
+        image: 'image/recipe3.jpg'
+    }),
+
+    new Recipe({
+        recipe_ID: 4,
+        recipe_Name: 'Vegetarian Lasagna',
+        item: '600g Chicken, 1/2 cup Yoghurt, 410g Tomato puree, 1/2 cup Cream, 1 Onion, 2 cloves Garlic, 1/4 teaspoon chilli powder',
+        instructions: '1. Place yoghurt, garlic and chilli powder into a dish. 2. Add chicken, stir to coat and refridgerate for 2 hours',
+        image: 'image/recipe4.jpg'
+    }),
+
+]
+
+var done =0;
+for(var i=0; i<recipes.length; i++){
+    recipes[i].save(function(err, result){
+        done++;
+        if(done === recipes.length){
+            exit();
+        }
+    });
+}
+
+function exit(){
+    mongoose.disconnect();
+}
