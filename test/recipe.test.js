2cf073cedde3af2f30bdc2805d4e8f3ad0150eec
brigzap <49735180+brigzap@users.noreply.github.com>
2019-10-18 08:50:28 +1100

    Delete recipe.test.js

--- test/recipe.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-const Recipes = require('../models/Recipes');
-const mongoose = require('mongoose')
-
-test('Load Page', async() => {
-    await request(app).post('/recipes/recipeslist').send({
-    }).expect(302)
-})
