068ad86e0cd88451e077aa41b7564b4346281e09
JoshChun <joshuachun@outlook.com>
2019-10-21 16:49:20 +1100

    Update notification.test.js

--- test/notification.test.js
+++ test/notification.test.js
@@ -1,37 +1,37 @@
-// const request = require('supertest');
-// const {ensureAuthenticated} = require('../helpers/auth');
-// const app = require('../app');
-// const User = require('../models/User');
-// const payment = require('../models/payment');
-// const mongoose = require('mongoose')
-// const databaseName = 'test'
+const request = require('supertest');
+const {ensureAuthenticated} = require('../helpers/auth');
+const app = require('../app');
+const User = require('../models/User');
+const payment = require('../models/payment');
+const mongoose = require('mongoose')
+const databaseName = 'test'
 
-// test('Should input payment details', async() => {
-//   const response =  await request(app).post('/payment').send({
-//     cardnum: '1234567891234567',
-//     expiredate: 'October 2019',
-//     securitycode: '1234',
-//     phonenum: '0412345678',
-//     emailaddress: 'joshuachun@outlook.com'
-//     }).expect(200)
-// })
+test('Should input payment details', async() => {
+const response =  await request(app).post('/payment').send({
+     cardnum: '1234567891234567',
+     expiredate: 'October 2019',
+     securitycode: '1234',
+     phonenum: '0412345678',
+     emailaddress: 'joshuachun@outlook.com'
+     }).expect(200)
+ })
 
-// test('Should not input incorrect card number', async() => {
-//     const response =  await request(app).post('/payment').send({
-//       cardnum: '1234',
-//       expiredate: 'October 2019',
-//       securitycode: '1234',
-//       phonenum: '0412345678',
-//       emailaddress: 'joshuachun@outlook.com'
-//       }).expect(200)
-//   })
+ test('Should not input incorrect card number', async() => {
+     const response =  await request(app).post('/payment').send({
+       cardnum: '1234',
+       expiredate: 'October 2019',
+       securitycode: '1234',
+       phonenum: '0412345678',
+       emailaddress: 'joshuachun@outlook.com'
+       }).expect(200)
+   })
 
-// test('Should get payment page', async() => {
-//     await request(app)
-//     .get('/payment').expect(200);
-// }) 
+ test('Should get payment page', async() => {
+     await request(app)
+     .get('/payment').expect(200);
+ }) 
  
-// afterEach(async () => {
-//     await User.deleteMany()
-//   })
+ afterEach(async () => {
+     await User.deleteMany()
+   })
 
